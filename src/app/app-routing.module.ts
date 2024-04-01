8aada5e052872b945f03f649e78aef2ddc7e1169
victor Okpanachi <okpanachivictor0@gmail.com>
2024-04-01 21:05:38 +0200

    home page and pages routing for movies list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesListComponent } from './pages/movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'movies-list', component: MoviesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
