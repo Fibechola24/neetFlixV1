74d3e02d859129aef11224ac6d6489dfa2c71be5
victor Okpanachi <okpanachivictor0@gmail.com>
2024-04-02 05:57:16 +0200

    styling of the slider component page with scss and icons from primeng

--- src/app/component/slider/slider.component.ts
+++ src/app/component/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-slider',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './slider.component.scss'
 })
 export class SliderComponent {
-
+  
 }
