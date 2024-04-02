16c590bd846ba0f4c488a9df9a67bebcc8206b7b
victor Okpanachi <okpanachivictor0@gmail.com>
2024-04-02 17:30:50 +0200

    renamed component as components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesListComponent } from './pages/movies-list/movies-list.component';
-import { SliderComponent } from './component/slider/slider.component';
+import { SliderComponent } from './components/slider/slider.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MoviesService } from './services/movies.service';
 
