985e700613b1378d777470665655094307f1df60
victor Okpanachi <okpanachivictor0@gmail.com>
2024-04-02 06:21:12 +0200

    show the json response of popular movies in frontend

--- src/app/component/slider/slider.component.ts
+++ src/app/component/slider/slider.component.ts
@@ -6,12 +6,19 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements 
+OnInit {
+
+  movies: any;
   constructor (private http: HttpClient) {}
 
   ngOnInit() {
+   this.getPopularMovies();
+  }
+
+  getPopularMovies(){
     this.http.get('https://api.themoviedb.org/3/person/popular?api_key=e957d81a112f250977b2bd17d6fe0e6f').subscribe((data)=>{
-      console.log(data)
+      this.movies = data;
     })
   }
 }
