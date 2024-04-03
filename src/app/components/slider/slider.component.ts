8d35f19f2fb5492af824109bcee2b47109336b89
victor Okpanachi <okpanachivictor0@gmail.com>
2024-04-03 15:29:19 +0200

    implemented get popular movies with api

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MoviesService } from '../../services/movies.service';
 import { threadId } from 'worker_threads';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-slider',
@@ -10,8 +11,15 @@ import { threadId } from 'worker_threads';
 })
 export class SliderComponent {
 
+
   constructor (private moviesService: MoviesService) {}
 
-    movies$ = this.moviesService.getPopularMovies();
+    movies$ = this.moviesService.getPopularMovies().pipe(
+      tap((x) => {
+        console.log(x);
+      })
+    );
+  
+    items = ['Name1', 'Name2', 'Name3', "Name4"];
   
 }
